e5133b4e641f72fcfdb7c43c821090730a352dac
Aaron Rogers <arogers@range-digital.com>
2022-06-15 12:50:13 -0500

    Initial commit.

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Aaron Rogers on 6/13/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+    }
+
+
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Aaron Rogers on 6/13/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+    }
+    
+
+}
